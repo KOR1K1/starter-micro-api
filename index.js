73198f1c5d115b07ffcb347deff386d48f8ba9b5
KOR1K <korobtsov.ds@gmail.com>
2024-04-11 22:00:06 +0700

    third changes

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
+import TOKEN from './token.js'
+const bot = new Telegraf(process.env.TELEGRAM_BOT || TOKEN)
+bot.use(async (ctx, next) => {
+	console.time(`Processing update ${ctx.update.update_id}`)
+	await next() // runs next middleware
+	// runs after next middleware finishes
+	console.timeEnd(`Processing update ${ctx.update.update_id}`)
+})
+
+bot.on(message('text'), ctx => ctx.reply('Hello World'))
+bot.launch()
+
+/*
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+*/
